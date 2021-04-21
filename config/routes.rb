f10516ee5bfdd37e167171d98d0748ca15aca02b
cccswann <cccswann@gmail.com>
2021-04-21 06:19:30 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  root to: 'sessions#welcome'
+  devise_for :users, controllers: { omniauth_callbacks: 'sessions' }
+  resources :users do 
+    resources :wishlists
+  end
+
+  resources :sessions
+  
+  resources :groupbuys do
+    collection do
+    end
+  end
+
+  #get 'users/sign_up' 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
