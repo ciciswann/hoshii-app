2e23dbb1f58851a3126b1f707cd1b63872fc64fd
cccswann <cccswann@gmail.com>
2021-04-21 06:01:34 -0700

    Create category model

---
+++ app/models/category.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Category < ApplicationRecord
+    has_many :groupbuys
+    has_many :wishlists, through: :groupbuys
+
+    accepts_nested_attributes_for :groupbuys
+
+    validates :name, presence: true, length: {minimum: 3}
+
+    def groupbuy_attributes=(groupbuys)
+        #binding.pry
+        groupbuys.values.each do |gb_hash|
+            self.groupbuys.push(Groupbuy.create(gb_hash))
+        end
+    end
+end
