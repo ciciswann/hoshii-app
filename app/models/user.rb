1defbb9bcc958b528d5c306f16039b7c5939ab54
Ciara Swann <cccswann@gmail.com>
2021-04-26 18:05:38 -0700

    Remove devise method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
     has_many :groupbuys, through: :wishlists
 
     devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
 
     # Validations
 
-    def self.create_from_provider_data(provider_data)
-        where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
-            user.email = provider_data.info.email
-            user.password = Devise.friendly_token[0, 20]
-            user.name = provider_data.info.name
+    def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+          user.email = auth.info.email
+          user.password = Devise.friendly_token[0, 20]
+          user.name = auth.info.name
         end
-    end
+      end
 end
