4c3be9e5e870877d18c78a92ca3528de759cc658
Ciara Swann <cccswann@gmail.com>
2021-04-26 18:02:43 -0700

    Create strong params and welcome.

--- app/controllers/groupbuys_controller.rb
+++ app/controllers/groupbuys_controller.rb
@@ -1,11 +1,22 @@
 class GroupbuysController < ApplicationController
+    before_action :authenticate_user!
+
+    def welcome
+        @groupbuys = Groupbuy.search(params[:search])
+        render :welcome
+    end
+    
     def index
-        render({json: Groupbuy.all})
+        @groupbuys = Groupbuy.all
+    end
+
+    def show
+
     end
 
     private
 
-    # def groupbuy_params
-    #     params.require
-    # end
+    def groupbuy_params
+        params.require(:groupbuy).permit(:name, :search)
+    end
 end
