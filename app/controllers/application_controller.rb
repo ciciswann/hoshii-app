ed22fbf0a9f7d17de2e4b71cc322e70ff7ae1edb
Ciara Swann <cccswann@gmail.com>
2021-04-30 15:51:27 -0700

    Add sign out path method to App Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    helper_method :current_user
     before_action :authenticate_user!
-
-    private
-
-    def current_user
-        @current_user ||= User.find(sessions[:user_id]) if session[:user_id]
+    
+    def after_sign_out_path_for(resource_or_scope)
+        request.referrer
     end
+
+    
+
 end
