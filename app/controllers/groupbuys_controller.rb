007cc491c170917f8d99e7b41f74f9e3f3fdb0d4
Ciara Swann <cccswann@gmail.com>
2021-04-30 16:29:56 -0700

    Add logic for show method.

--- app/controllers/groupbuys_controller.rb
+++ app/controllers/groupbuys_controller.rb
@@ -1,5 +1,5 @@
 class GroupbuysController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, only: [:index, :new, :create]
 
     def welcome
         @groupbuys = Groupbuy.where(nil) # anonymous scope
@@ -10,6 +10,9 @@ class GroupbuysController < ApplicationController
     end
     
     def index
+        @user = User.find_by(session[:id])
+        @user_wishlists = @user.wishlists
+        @wishlist = Wishlist.find_by_id(params[:wishlist_id])
         @groupbuys = Groupbuy.where(nil) # anonymous scope
         @groupbuys = @groupbuys.search(params[:search]) if params[:search].present?
         @groupbuys = @groupbuys.filter_by_status(params[:status]) if params[:status].present?
@@ -18,7 +21,8 @@ class GroupbuysController < ApplicationController
     end
 
     def show
-
+        @groupbuy = Groupbuy.find(params[:id])
+        render :show
     end
 
     private
