313384d36a5f7ca59cf450a5fe94c4297b953bc3
Ciara Swann <cccswann@gmail.com>
2021-05-01 15:53:39 -0700

    Create Users Controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,12 @@
 class UsersController < ApplicationController
-    #before_action :authorize, only: [:show]
-
-    def new
-        @user = User.new
-    end
-
-    def edit
-    end
-    
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to '/'
-        else
-            render 'new'
-        end
-    end
+    before_action :authenticate_user!
 
     def index
-        @users = User.all
+
     end
 
     def show
-       if !!current_user
-            @user = User.find_by(params[:id])
-       else
-            redirect_to '/'
-       end
+        
     end
 
-    private
-
-    def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
 end
