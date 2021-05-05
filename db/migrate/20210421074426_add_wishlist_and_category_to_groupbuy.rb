827639cb35c0efb9d6d0f091ba66e331d79b6f2b
Ciara Swann <cccswann@gmail.com>
2021-05-04 18:04:41 -0700

    Change foreign key on delete to nullify.

--- db/migrate/20210421074426_add_wishlist_and_category_to_groupbuy.rb
+++ db/migrate/20210421074426_add_wishlist_and_category_to_groupbuy.rb
@@ -1,6 +1,6 @@
 class AddWishlistAndCategoryToGroupbuy < ActiveRecord::Migration[6.1]
   def change
-    add_reference :groupbuys, :wishlist, null: true, foreign_key: true
-    add_reference :groupbuys, :category, null: true, foreign_key: true
+    add_reference :groupbuys, :wishlist, foreign_key: {on_delete: :nullify}
+    add_reference :groupbuys, :category, null: true, foreign_key: {on_delete: :nullify}
   end
 end
