70247283c758b753dc2bc754328799b28b35e4fa
Ciara Swann <cccswann@gmail.com>
2021-05-04 17:59:28 -0700

    Clean up controller.

--- app/controllers/groupbuys_controller.rb
+++ app/controllers/groupbuys_controller.rb
@@ -1,22 +1,24 @@
 class GroupbuysController < ApplicationController
     before_action :authenticate_user!, only: [:index, :new, :create]
 
+    ORDERS = [ "pricing ASC", "pricing DESC", "name ASC", "name DESC" ]
+
     def welcome
-        @groupbuys = Groupbuy.where(nil) # anonymous scope
-        @groupbuys = @groupbuys.search(params[:search]) if params[:search].present?
-        @groupbuys = @groupbuys.filter_by_status(params[:status]) if params[:status].present?
-        @groupbuys = @groupbuys.sort_by_price(params[:price]) if params[:price].present?
+        @groupbuys = Groupbuy.all 
         render :welcome
     end
     
     def index
-        @user = User.find_by(session[:id])
+        @user = current_user
         @user_wishlists = @user.wishlists
-        @wishlist = Wishlist.find_by_id(params[:wishlist_id])
+        @wishlist = @user_wishlists.find_by(id: params[:wishlist_id])
         @groupbuys = Groupbuy.where(nil) # anonymous scope
         @groupbuys = @groupbuys.search(params[:search]) if params[:search].present?
         @groupbuys = @groupbuys.filter_by_status(params[:status]) if params[:status].present?
-        @groupbuys = @groupbuys.sort_by_price(params[:price]) if params[:price].present?
+        @groupbuys = @groupbuys.filter_by_cat(params[:category_id]) if params[:category_id].present?
+        if params[:ordering] && ordering = ORDERS[params[:ordering].to_i]
+            @groupbuys = @groupbuys.order(ordering)
+        end
         render :browse
     end
 
